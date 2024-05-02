1ef3893e566393e759f48cff79b2ca9093e596cc
PRAGUN_BHUTANI <1109752+pragunbhutani@users.noreply.github.com>
2024-05-02 19:20:28 +0530

    changed to absolute paths

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,5 @@
-import Footer from "@components/Footer";
-import Logo from "@components/Logo";
+import Footer from "./components/Footer";
+import Logo from "./components/Logo";
 
 import { DocsThemeConfig } from "nextra-theme-docs";
 
