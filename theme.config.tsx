df42e66548a44cf9330ec8b6e8452b4aaab0f675
PRAGUN_BHUTANI <1109752+pragunbhutani@users.noreply.github.com>
2024-05-03 11:11:36 +0530

    updated opengraph image again

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,8 @@
 import Footer from "@components/Footer";
 import Logo from "@components/Logo";
 
+import darkIcon from "@public/img/icon-dark.png";
+
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
@@ -44,7 +46,7 @@ const config: DocsThemeConfig = {
           "A collection of design principles, strategies and frameworks we use to build data infrastructure and set up data teams.",
         images: [
           {
-            url: "/public/img/icon-dark.png",
+            url: darkIcon.src,
             width: 1200,
             height: 1200,
             alt: "Bottom Line Data Handbook",
