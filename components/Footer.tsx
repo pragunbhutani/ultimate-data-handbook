17719e055c8c2f2f544dab97b5fa43a3b677dc4e
PRAGUN_BHUTANI <1109752+pragunbhutani@users.noreply.github.com>
2024-06-15 22:24:52 +0530

    updated product analytics

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="text-sm text-gray-500">
       <div className="mx-auto max-w-8xl space-y-2 px-6 py-12 md:flex md:items-center md:justify-between lg:px-8 text-center">
-        <div className="md:order-2">
+        <div className="md:order-3">
           <p>
             Written and maintained with ❤️ by{" "}
             <a
@@ -13,6 +13,9 @@ export default function Footer() {
               Pragun Bhutani
             </a>
           </p>
+        </div>
+        <div className="md:order-2">
+          <p>All views expressed are my own.</p>
         </div>
         <div className="md:order-1 md:mt-0">
           <p className="leading-5">
